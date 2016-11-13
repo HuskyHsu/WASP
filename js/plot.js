7fa083584eef254b80cc5d253d6d4bd8d31bc469
HuskyHsu <chessman416@hotmail.com>
2016-11-13 18:27:20 +0800

    readfile

---
+++ js/plot.js
@@ -0,0 +1,92 @@
+﻿(function(window, document, undefined) {
+	//Class起始設定
+	var LineCharts = function(id, data) {
+
+		var margin = {
+			top: 20,
+			right: 50,
+			bottom: 30,
+			left: 50
+		};
+		var width = 960 - margin.left - margin.right;
+		var height = 500 - margin.top - margin.bottom;
+
+		this.svg = d3.select(id)
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom);
+
+		this.path = this.svg.append("g")
+			.append("path")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+			.datum(data)
+			.attr("class", "line");
+
+		//this.data = data;
+
+		var x = d3.scaleLinear() //scalePoint
+			.domain(d3.extent(data, function(d) {
+				return d[0];
+			}))
+			.range([0, width]);
+
+		this.y = d3.scaleLinear()
+			.range([height, 0]);
+
+		//var x = this.x;
+
+		this.line = d3.line()
+			.x(function(d) {
+				return x(d[0])
+			});
+
+
+		//axis
+		this.axis = this.svg.append("g")
+			.attr("class", "axis");
+
+		this.axis.append("g")
+			.attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
+			.attr("class", "axis axis--x")
+			.call(d3.axisBottom(x));
+
+		this.axis.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+			.attr("class", "axis axis--y");
+
+		this.axis.append("text")
+			.attr("y", margin.top + height)
+			.attr("x", margin.left + width)
+			.attr("dy", "3.2em")
+			.style("text-anchor", "end")
+			.text(this.data2_name);
+
+	};
+
+	//線
+	LineCharts.prototype.linrre = function(site) {
+
+		//data.splice(150, data.length);
+
+		var y = this.y.domain([0, d3.max(data, function(d) {
+			return d[site];
+		})]);
+
+		this.line.y(function(d) {
+			return y(d[site])
+		});
+
+		this.path
+			.attr("d", this.line);
+
+		this.axis.selectAll("g.axis.axis--y").call(d3.axisLeft(y));
+
+
+
+	};
+
+
+	window.Charts = {
+		Line: LineCharts
+	};
+
+})(window, document);
---
+++ js/readfile.js
@@ -0,0 +1,56 @@
+﻿(function(window, document, undefined) {
+
+	$(document).ready(function() {
+
+		$(':file').change(function() {
+			var file = this.files[0];
+			//var name = file.name;
+			//var size = file.size;
+			var type = file.type;
+
+			//NO CSV!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			if (!type.match(/application\/vnd.ms-excel|application\/csv/))
+				return;
+
+			var fReader = new FileReader();
+			fReader.onload = function(event) {
+				var data = event.target.result;
+
+
+				data = data.split('\n');
+				data.splice(0, 1);
+				data.forEach(function(item, index){
+					var tem = data[index].split(',');
+					tem.forEach(function(item_, index_){
+						tem[index_] = tem[index_].trim() - 0;
+					});
+					tem.pop();
+					data[index] = tem;
+				});
+
+				document.querySelector('[name=Text1]').innerHTML = '模擬天數：' + data[data.length - 1][0] + ' 天';
+
+				window.data = data;
+
+				window.plot = new window.Charts.Line('#plot', data);
+				//plot.line(1);
+			};
+
+			fReader.readAsText(file);
+
+		});
+
+		$('#plotGo').click(function(){
+			var line = document.querySelector('#line option:checked').value - 0;
+			window.plot.linrre(line);
+
+		});
+	})
+
+
+
+	$(window).on('load', function(e) {
+
+	})
+
+})(window, document);
