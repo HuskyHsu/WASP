2d6792c98d82dca29e4b3630d85996e95c1aabcb
Shih-Chi, Hsu <chessman416@hotmail.com>
2016-11-24 10:57:59 +0800

    修正color bar bug

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ var vm = new Vue({
     },
     watch: {
     	type: function (val) {
+            window.mapObj.map.addControl(new window.colorBarControl());
+
       		window.chart.draw(vm.site);
+
+            var dx = (data[window.chart.focusIndex][vm.site][vm.type] / data.max[vm.type] * 100) * 3;
+            d3.select("#colorBar rect").attr("transform", "translate(" + (dx + 23.5) + ", 8)");
 
             var col = window.data.colmap[vm.type];
 			window.mapObj.SKCGroup.eachLayer(function(circle) {
@@ -16,7 +21,6 @@ var vm = new Vue({
                 });
             });
 
-			window.mapObj.map.addControl(new window.colorBarControl());
     	}
   	}
 })
